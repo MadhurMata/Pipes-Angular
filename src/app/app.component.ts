3f39b0a7c68fa3e68c34bc05c29320d17c4838fb
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-09-02 10:51:05 +0200

    currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+name = "Fernando";
+
+arr = [1,2,3,4,5,6,7,8,9,10];
+
+PI = Math.PI;
+
+a = 0.234;
+
+salary = 1234.5 ;
+
 }
